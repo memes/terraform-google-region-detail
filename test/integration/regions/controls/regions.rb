04d0e823cd63c73785ea9819beeec60c1238cbf6
Matthew Emes <memes@matthewemes.com>
2023-02-20 10:31:16 -0800

    feat: Add known IPv4 and IPv6 blocks to details
    Google publishes a set of IPv4 and IPv6 CIDR blocks that are used for
    public IP addreses in each Compute Engine region. This commit adds those
    values to the detail results in this module.
    
    Closes #5

--- test/integration/regions/controls/regions.rb
+++ test/integration/regions/controls/regions.rb
@@ -1,9 +1,18 @@
 # frozen_string_literal: true
 
+require 'json'
+
 control 'regions' do
   title 'Ensure regions module results match expectations'
   impact 1.0
-  results = JSON.parse(input('output_results_json'), { symbolize_names: true })
+  # Remove IPv4/IPv6 values from the results since those are tested in the other
+  # controls
+  results = JSON.parse(input('output_results_json'),
+                       { symbolize_names: true }).transform_values do |value|
+    value.reject do |k, _|
+      %i[ipv4 ipv6].include?(k)
+    end
+  end
   expected = JSON.parse(input('output_expected_json'), { symbolize_names: true })
 
   describe results do
